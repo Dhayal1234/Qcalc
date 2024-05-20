82913e00c23a9edc92eef77b391a8a0003f9e731
Dhayal1234 <ankitdhayal00004@gmail.com>
2024-05-21 00:57:24 +0530

    ME_QCALC_QA_MODULE_INHERITANCE  (Crio.Do)

--- app/src/main/java/com/crio/qcalc/StandardCalculator.java
+++ app/src/main/java/com/crio/qcalc/StandardCalculator.java
@@ -4,7 +4,7 @@ public class StandardCalculator {
 
 
 
-    private double result;
+    protected double result;
 
 
     public double getResult() {
@@ -43,7 +43,7 @@ public void add(double num1, double num2){
 }
 
         
-    public void add(int num1, int num2){
+    public final void add(int num1, int num2){
 
         add((double)num1, (double)num2);
 
@@ -53,7 +53,7 @@ public void add(double num1, double num2){
 
     
     
-    public void subtract(int num1, int num2){
+    public final void subtract(int num1, int num2){
 
         subtract((double)num1, (double)num2);
 
@@ -76,7 +76,7 @@ public void subtract(double num1, double num2){
 
    
         
-    public void multiply(int num1, int num2){
+    public final void multiply(int num1, int num2){
 
         multiply((double)num1, (double)num2);
 
@@ -87,7 +87,7 @@ public void subtract(double num1, double num2){
     }
 
 
-    public void divide(int num1, int num2)
+    public final void divide(int num1, int num2)
     {
         result = num1/num2 ;
     }
